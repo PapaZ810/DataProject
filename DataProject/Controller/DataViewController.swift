cf6b63cb4abb4f032f3206be5a7803f5db71fff0
Zac <zombiezac@att.net>
2018-11-26 12:20:03 -0700

    initial commit

---
+++ DataProject/Controller/DataViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  DataProject
+//
+//  Created by Durham, Zachary on 11/26/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+import UIKit
+
+public class DataViewController: UIViewController
+{
+
+    public override func viewDidLoad() -> Void
+    {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
