40b5f7e4c6bfe2ca7b9df3ad2f1a9dfa5d006f11
Zac <zombiezac@att.net>
2018-11-30 12:36:46 -0700

    added a view and some information in that table view

---
+++ DataProject/Controller/BucketItemCell.swift
@@ -0,0 +1,27 @@
+//
+//  BucketItemCell.swift
+//  DataProject
+//
+//  Created by Durham, Zachary on 11/30/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+import UIKit
+
+public class BucketItemCell: UITableViewCell
+{
+
+    public override func awakeFromNib()
+    {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    public override func setSelected(_ selected: Bool, animated: Bool)
+    {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
